681607d4359fab09898faacb3da3e991d4eafce8
coxxlongg <kubenova.aruzhan2005@gmail.com>
2024-02-18 17:19:42 +0600

    changes

--- lab4/json/json1.py
+++ lab4/json/json1.py
@@ -14,17 +14,13 @@ def format_interface_status(data):
     column_titles = f"{'DN':<50} {'Description':<20} {'Speed':<8} {'MTU':<6}"
     print(f"{header}\n{separator}\n{column_titles}\n{'-' * 80}")
 
-    # Iterate through each item in the 'imdata' list
     for item in data['imdata']:
-        # Extract the necessary attributes
         attributes = item['l1PhysIf']['attributes']
         dn = attributes.get('dn', '')
         descr = attributes.get('descr', '')
         speed = attributes.get('speed', '')
         mtu = attributes.get('mtu', '')
 
-        # Print each row of data
         print(f"{dn:<50} {descr:<20} {speed:<8} {mtu:<6}")
 
-# Call the function with the loaded JSON data
 format_interface_status(data)
