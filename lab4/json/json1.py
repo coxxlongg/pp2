88e1766cc92f75d1d4b30935cd64f5e46215be8b
coxxlongg <kubenova.aruzhan2005@gmail.com>
2024-02-19 12:14:01 +0600

    chnges1

--- lab4/json/json1.py
+++ lab4/json/json1.py
@@ -1,26 +1,18 @@
 import json
 
-file_path = '/mnt/data/sample-data.json'
+print(
+    "Interface Status\n"
+    "================================================================================\n"
+    "DN                                                 Description           Speed    MTU\n"
+    "-------------------------------------------------- --------------------  ------  ------"
+)
 
-with open(file_path, 'r') as file:
-    data = json.load(file)
+with open("sample-data.json", "r") as info:
+    info = json.load(info)
 
-data_slice = data[:3] if isinstance(data, list) else data 
-data_slice
-
-def format_interface_status(data):
-    header = f"{'Interface Status':^80}"
-    separator = "=" * 80
-    column_titles = f"{'DN':<50} {'Description':<20} {'Speed':<8} {'MTU':<6}"
-    print(f"{header}\n{separator}\n{column_titles}\n{'-' * 80}")
-
-    for item in data['imdata']:
-        attributes = item['l1PhysIf']['attributes']
-        dn = attributes.get('dn', '')
-        descr = attributes.get('descr', '')
-        speed = attributes.get('speed', '')
-        mtu = attributes.get('mtu', '')
-
-        print(f"{dn:<50} {descr:<20} {speed:<8} {mtu:<6}")
-
-format_interface_status(data)
+    for i in info['imdata']:
+        print(i["l1PhysIf"]["attributes"]["dn"], "\t\t\t\t",
+              i["l1PhysIf"]["attributes"]["descr"], "\t\t\t",
+              i["l1PhysIf"]["attributes"]["speed"], "\t\t",
+              i["l1PhysIf"]["attributes"]["mtu"]
+              )
